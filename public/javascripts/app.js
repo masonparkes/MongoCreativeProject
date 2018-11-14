bfda548344de5485db3ef2e2e9a285be66286148
Mason <masonlparkes@gmail.com>
2018-11-14 20:53:33 +0000

    a few changes that will make chasethebox work better

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,6 +12,11 @@ $(document).ready(function() {
         });
 
     });
+    /*
+    //I'm not sure this would be needed
+    //I was worried about potential problems with going to different pages for the games and then
+    //trying to access the databases from those pages, and organizing functions and all that, but I think
+    //we should be able to make it work. 
     $("#viewRTC").click(function() {
         //get high scores from database
         //put them in the highscore div
@@ -24,4 +29,97 @@ $(document).ready(function() {
             $("#highscorelist").html(list);
         });
     });
+    $("#playRTC").click(function() {
+        var newHTML=getGamehtml("RTC");
+        $("#game").html(newHTML);
+        $("#start").click(startGame());
+    })
+    */
 });
+/*
+//same as above, we would only need these if we were going to change how the game ran
+function getGamehtml(game_name) {
+    if (game_name == "RTC") {
+        var stuff = "";
+        stuff += '<button type = "button" id = "start">Start</button>';
+        stuff += '<div id="game-window" style="display:none">';
+        stuff += '<p id="mover">&#9658</p> </div>';
+        stuff += '<div id="instructions" style="display:none">';
+        stuff += '<p>Click the button to move the arrow, make it to the other side before time runs out.</p>';
+        stuff += '<button type="button" id="move" onclick=move()>GO</button></div>';
+        stuff += '<div id="win" style="display:none"><h1 class="message">You Won!!</h1></div>';
+        stuff += '<div id="game-over" style="display:none"><h class="message">Should have moved faster</h></div>';
+        stuff += '<button id="again" type="button" style="display:none" onclick=reset()>Play Again</button>';
+        return stuff;
+    }
+    else if(game_name=="CTB"){
+        
+    }
+
+}
+//functions for Race the clock game
+function startGame()
+         {
+             start_time=new Date().getTime()/1000;
+             document.getElementById('game-window').style.display="block";
+             document.getElementById('instructions').style.display="block";
+             
+             
+         }
+         function endGame()
+         {
+             document.getElementById('instructions').style.display="none";
+             document.getElementById('game-window').style.display="none";
+             document.getElementById('game-over').style.display="block";
+             document.getElementById('again').style.display="block";
+            
+         }
+         function getOffset( el )
+         {
+            var _x = 0;
+            var _y = 0;
+            while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+            _x += el.offsetLeft - el.scrollLeft;
+            _y += el.offsetTop - el.scrollTop;
+            el = el.offsetParent;
+            }
+            return { top: _y, left: _x };
+         }
+         function move()
+         {
+             end_time=new Date().getTime()/1000;
+             time=end_time-start_time;
+             if(time>20)
+             {
+                 lose();
+             }
+             var x=getOffset( document.getElementById('mover') ).left;
+             var y=x+20+"px"
+             document.getElementById('mover').style.left=y;
+             if(x+20>=1180)
+             {
+                 win();
+             }
+         }
+         function win()
+         {
+             
+            alert("you won! Your time was "+time+" seconds! Click 'ok' to play again");
+            reset();
+         }
+         function reset()
+         {
+             document.getElementById('game-window').style.display="none";
+             document.getElementById('instructions').style.display="none";
+             document.getElementById('mover').style.left="80px";
+
+             startGame();
+            
+
+         }
+         function lose()
+         {
+             alert("you didn't make it. Better luck next time... Click 'ok' to play again");
+             reset();
+         }
+*/
